f068f6ee8f514c5ea73add729c6dd4992db80e8c
noriyotcp <noriyo.akita@gmail.com>
2017-08-31 20:37:11 +0900

    Chapter 4: Building Tests

--- chapter-04.md
+++
@@ -1,6 +0,0 @@
-# Chapter 4: Building Tests
-
-
-## The Value of Self-Testing Code
-## The Test::Unit Testing Framework
-## Developer and Quality Assurance Tests Adding More Tests
---
+++ chapter-04/file_test.rb
@@ -0,0 +1,42 @@
+# Chapter 4: Building Tests
+require 'minitest/autorun'
+
+class TestFile < MiniTest::Test
+  def test_read_4th_character
+    contents = File.read('data.txt')
+    assert_equal('d', contents[3, 1])
+  end
+
+  def test_read_4th_content_is_2
+    contents = File.read('data.txt')
+    assert_equal('2', contents[3, 1])
+  end
+
+  def test_read_causes_error_when_file_not_found
+    contents = File.read('datas.txt')
+    assert_equal('2', contents[3, 1])
+  end
+
+  def test_read_raises_error_when_file_not_found
+    assert_raises Errno::ENOENT do
+      File.read('datas.txt')
+    end
+  end
+
+  def test_read_with_a_length_specified
+    contents = File.read('data.txt', 15)
+    assert_equal('Bradman 99.', contents)
+  end
+
+  def test_read_empty_file_returns_empty_string
+    File.open('empty_data.txt', 'w') { }
+    contents = File.read('empty_data.txt')
+    assert_equal '', contents
+  end
+
+  def test_read_an_out_bounds_length_causes_no_error
+    File.open('simple_data.txt', "w") { |file| file << 'simple file' }
+    contents = File.read('simple_data.txt', 100)
+    assert_equal 'simple file', contents
+  end
+end
